bab0fdb1dfbcedc4790a1ba01b5e166df50149d2
Asavei Florin <asaveiflorin@gmail.com>
2020-05-02 13:46:58 +0300

    created simple navbar

--- landing-page/src/app/app-routing.module.ts
+++ landing-page/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'acasa', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '',   redirectTo: '/acasa', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
