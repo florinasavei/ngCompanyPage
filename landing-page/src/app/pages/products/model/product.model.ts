3510680ff109f94be44c0d761ae2b3be49ca6e84
Florin Asavei <asaveiflorin@gmail.com>
2020-05-10 18:55:19 +0300

    added more products and fixed header on mobile

--- landing-page/src/app/pages/products/model/product.model.ts
+++ landing-page/src/app/pages/products/model/product.model.ts
@@ -1,3 +1,4 @@
 export class Product {
     images: string[];
+    name: string;
 }
--- landing-page/src/app/pages/products/products.component.ts
+++ landing-page/src/app/pages/products/products.component.ts
@@ -16,35 +16,60 @@ export class ProductsComponent implements OnInit {
 
   products: Product[] = [
     {
-      images : ['../../assets/img/products/cash-registers/SuccesM4/SuccesM4.png', '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_2.png',
-      '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_3.png']
+      name: "Success M4",
+      images: [
+        '../../assets/img/products/cash-registers/SuccesM4/SuccesM4.png',
+        '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_2.png',
+        '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_3.png',
+      ]
     },
     {
-      images : ['../../assets/img/products/cash-registers/SuccesM4/SuccesM4.png', '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_2.png',
-      '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_3.png']
+      name: "Success M7",
+      images: [
+        '../../assets/img/products/cash-registers/SuccesM7/succes-m7-negru.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SeccesM7_alb.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SeccesM7_albastru.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SeccesM7_albastru_2.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SeccesM7_trio.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SeccesM7_turquoise.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SuccesM7_rosu_lateral.png',
+      ]
     },
     {
-      images : ['../../assets/img/products/cash-registers/SuccesM4/SuccesM4.png', '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_2.png',
-      '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_3.png']
+      name: "Custom KubeIIX-F",
+      images: [
+        '../../assets/img/products/cash-registers/CustomKubeIIX-F/KubeX.png',
+         '../../assets/img/products/cash-registers/CustomKubeIIX-F/KubeX_caricam-carta.png',
+        '../../assets/img/products/cash-registers/CustomKubeIIX-F/KUBEX+mano+giornale-elettr.png']
     },
     {
-      images : ['../../assets/img/products/cash-registers/SuccesM4/SuccesM4.png', '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_2.png',
-      '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_3.png']
+      name: "Success M4",
+      images: [
+        '../../assets/img/products/cash-registers/SuccesM4/SuccesM4.png',
+        '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_2.png',
+        '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_3.png',
+      ]
     },
     {
-      images : ['../../assets/img/products/cash-registers/SuccesM4/SuccesM4.png', '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_2.png',
-      '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_3.png']
+      name: "Success M7",
+      images: [
+        '../../assets/img/products/cash-registers/SuccesM7/succes-m7-negru.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SeccesM7_alb.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SeccesM7_albastru.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SeccesM7_albastru_2.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SeccesM7_trio.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SeccesM7_turquoise.png',
+        '../../assets/img/products/cash-registers/SuccesM7/SuccesM7_rosu_lateral.png',
+      ]
     },
     {
-      images : ['../../assets/img/products/cash-registers/SuccesM4/SuccesM4.png', '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_2.png',
-      '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_3.png']
+      name: "Custom KubeIIX-F",
+      images: [
+        '../../assets/img/products/cash-registers/CustomKubeIIX-F/KubeX.png',
+         '../../assets/img/products/cash-registers/CustomKubeIIX-F/KubeX_caricam-carta.png',
+        '../../assets/img/products/cash-registers/CustomKubeIIX-F/KUBEX+mano+giornale-elettr.png']
     },
-  ]
-
-  product: Product = {
-    images : ['../../assets/img/products/cash-registers/SuccesM4/SuccesM4.png', '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_2.png',
-    '../../assets/img/products/cash-registers/SuccesM4/SuccesM4_3.png']
-  };
+  ];
 
 
 }
