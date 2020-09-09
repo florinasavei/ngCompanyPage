b5d2b2244359244f04f1d2ff7102b219e291dc97
Florin Asavei <asaveiflorin@gmail.com>
2020-09-09 20:26:55 +0300

    added sample components

--- landing-page-ten/src/app/app-routing.module.ts
+++ landing-page-ten/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'acasa', component: HomeComponent },
+  { path: 'produse', component: ProductsComponent },
+  { path: '',   redirectTo: '/acasa', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- landing-page-ten/src/app/app.module.ts
+++ landing-page-ten/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
